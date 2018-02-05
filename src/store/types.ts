d1e0422bfb9c06f35db7b070d5b59011c788ed79
Eric Tran <me@erictran.ca>
2018-02-04 23:54:31 -0500

    Add Market Madness to the list of projects

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,63 +1,77 @@
 import { Link, Project, ProjectTags, OpenSourceContribution } from '../types';
 
 export type State = {
-    socialLinks: Link[],
-    projects: Project[],
-    selectedProject: SelectedProject | undefined;
-    openSourceContributions: OpenSourceContribution[];
+  socialLinks: Link[];
+  projects: Project[];
+  selectedProject: SelectedProject | undefined;
+  openSourceContributions: OpenSourceContribution[];
 };
 
 export type SelectedProject = {
-    project: Project;
-    index: number;
+  project: Project;
+  index: number;
 };
 
 export const initialState: State = {
-    socialLinks: [{
-        text: 'Twitter',
-        url: 'https://twitter.com/EricTran5791'
-    }, {
-        text: 'GitHub',
-        url: 'https://github.com/EricTran5791'
-    }, {
-        text: 'LinkedIn',
-        url: 'https://www.linkedin.com/in/erictran5791'
-    }],
-    projects: [{
-        name: 'The Improv Embassy',
-        url: 'http://www.improvembassy.com',
-        description:
-            `The Improv Embassy offers regular classes and shows for improv and sketch comedy.
+  socialLinks: [
+    {
+      text: 'Twitter',
+      url: 'https://twitter.com/EricTran5791',
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/EricTran5791',
+    },
+    {
+      text: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/erictran5791',
+    },
+  ],
+  projects: [
+    {
+      name: 'The Improv Embassy',
+      url: 'http://www.improvembassy.com',
+      description: `The Improv Embassy offers regular classes and shows for improv and sketch comedy.
             Highly customized and responsive components include a blogging platform, an events
             calendar, and an e-commerce shop.`,
-        imageUrl: '/static/images/project-ie.png',
-        tags: [ProjectTags.WordPress, ProjectTags.WooCommerce]
-    }, {
-        name: 'Ottawa Improv Festival',
-        url: 'http://www.ottawaimprovfestival.com/',
-        description:
-            `The annual Ottawa Improv Festival features a variety of local and out-of-town acts.
+      imageUrl: '/static/images/project-ie.png',
+      tags: [ProjectTags.WordPress, ProjectTags.WooCommerce],
+    },
+    {
+      name: 'Ottawa Improv Festival',
+      url: 'http://www.ottawaimprovfestival.com/',
+      description: `The annual Ottawa Improv Festival features a variety of local and out-of-town acts.
             The website is a central hub for festival information, ticket sales, and workshop registrations.`,
-        imageUrl: '/static/images/project-oif.png',
-        tags: [ProjectTags.WordPress]
-    }, {
-        name: 'Length Weight Converter',
-        url: 'https://play.google.com/store/apps/details?id=ca.erictran.lengthweightconverter',
-        description:
-            `This was my first Android app.
+      imageUrl: '/static/images/project-oif.png',
+      tags: [ProjectTags.WordPress],
+    },
+    {
+      name: 'Length Weight Converter',
+      url:
+        'https://play.google.com/store/apps/details?id=ca.erictran.lengthweightconverter',
+      description: `This was my first Android app.
             The premise is simple: to convert length and weight measurements.
             Overall, it was a great learning experience and I still use the app every now and then.`,
-        imageUrl: '/static/images/project-lwc.png',
-        tags: [ProjectTags.Android]
-    }, {
-        name: 'Personal Website',
-        url: 'https://github.com/EricTran5791/eric-tran',
-        description:
-            `My personal website built using Vue.
+      imageUrl: '/static/images/project-lwc.png',
+      tags: [ProjectTags.Android],
+    },
+    {
+      name: 'Personal Website',
+      url: 'https://github.com/EricTran5791/eric-tran',
+      description: `My personal website built using Vue.
             My website also leverages Vuex, CSS Grid, and the GitHub GraphQL API.`,
-        imageUrl: '/static/images/project-et.png',
-        tags: [ProjectTags.Vue]
-    }],
-    selectedProject: undefined,
-    openSourceContributions: []
+      imageUrl: '/static/images/project-et.png',
+      tags: [ProjectTags.Vue],
+    },
+    {
+      name: 'Market Madness',
+      url: 'https://github.com/EricTran5791/market-madness',
+      description: `A head-to-head deck building game built with React and MobX State Tree.
+            Work in progress.`,
+      imageUrl: '/static/images/project-mm.png',
+      tags: [ProjectTags.React],
+    },
+  ],
+  selectedProject: undefined,
+  openSourceContributions: [],
 };
--- src/types.ts
+++ src/types.ts
@@ -1,36 +1,37 @@
 export type Link = {
-    text?: string;
-    url: string;
+  text?: string;
+  url: string;
 };
 
 export type Project = {
-    name: string;
-    url: string;
-    description: string;
-    imageUrl: string;
-    tags: ProjectTags[];
+  name: string;
+  url: string;
+  description: string;
+  imageUrl: string;
+  tags: ProjectTags[];
 };
 
 export const enum ProjectTags {
-    Android = 'Android',
-    Vue = 'Vue',
-    WordPress = 'WordPress',
-    WooCommerce = 'WooCommerce'
+  Android = 'Android',
+  React = 'React',
+  Vue = 'Vue',
+  WordPress = 'WordPress',
+  WooCommerce = 'WooCommerce',
 }
 
 export const enum TransitionClasses {
-    None = '',
-    SlideIn = 'slide-in'
+  None = '',
+  SlideIn = 'slide-in',
 }
 
 export const enum OpenSourceContributionType {
-    Commit = 'Commit',
-    Issue = 'Issue Report'
+  Commit = 'Commit',
+  Issue = 'Issue Report',
 }
 
 export type OpenSourceContribution = {
-    repositoryName: string;
-    type: OpenSourceContributionType;
-    date: string;
-    url: string;
+  repositoryName: string;
+  type: OpenSourceContributionType;
+  date: string;
+  url: string;
 };
